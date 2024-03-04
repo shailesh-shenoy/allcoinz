d3188a25a82e2c2e6ce67e8fe8817657d510dbe3
shailesh-shenoy <shenoy.shailesh97@gmail.com>
2024-03-04 01:15:47 -0500

    Working migrations!

--- db/postgres.go
+++ db/postgres.go
@@ -2,7 +2,9 @@ package db
 
 import (
 	"database/sql"
+	"embed"
 	"fmt"
+	"io/fs"
 
 	_ "github.com/lib/pq"
 )
@@ -19,6 +21,9 @@ func NewDataStore(dsrc string) *DataStore {
 		dataSource: dsrc,
 	}
 }
+
+//go:embed migrations/*.sql
+var migrationFS embed.FS
 
 // Opens a database connection, returns error if unsuccessful
 func (ds *DataStore) Open() (err error) {
@@ -39,5 +44,61 @@ func (ds *DataStore) Open() (err error) {
 }
 
 func (ds *DataStore) migrate() (err error) {
+	// Ensure the 'migrations' table exists so we don't duplicate migrations.
+	if _, err := ds.db.Exec(`CREATE TABLE IF NOT EXISTS migrations (name TEXT PRIMARY KEY);`); err != nil {
+		return fmt.Errorf("cannot create migrations table: %w", err)
+	}
+
+	// Read migration files from our embedded file system.
+	names, err := fs.Glob(migrationFS, "migrations/*.sql")
+	if err != nil {
+		return err
+	}
+
+	// Loop over migration files and attempt to migrate each one
+	for _, name := range names {
+		if err := ds.migrateFile(name); err != nil {
+			return fmt.Errorf("migration failed for file: %q with error: %w", name, err)
+		}
+	}
+
 	return nil
+}
+
+func (ds *DataStore) migrateFile(name string) (err error) {
+	tx, err := ds.db.Begin()
+	if err != nil {
+		return err
+	}
+	// Rollback the transaction before returning.
+	// If there is an error, all SQL in migration file is rolled back.
+	// If there is no error, the transaction was already committed
+	// and the rollback is a NO-OP as there is nothing to rollback.
+	// * This is the reason there are strange empty rollbacks when using ORMs.
+	defer tx.Rollback()
+
+	// Ensure that the migration has not already been run.
+	// The below query returns n > 0 if migration was already run and inserted in the DB.
+	var n int
+	if err = tx.QueryRow(`SELECT COUNT(*) FROM migrations WHERE name = $1;`, name).Scan(&n); err != nil {
+		return err
+	}
+	if n != 0 {
+		// Migration was already run for this file, skip
+		return nil
+	}
+
+	// Read and execute migration file
+	if buf, err := fs.ReadFile(migrationFS, name); err != nil {
+		return err
+	} else if _, err = tx.Exec(string(buf)); err != nil {
+		return err
+	}
+
+	if _, err = tx.Exec(`INSERT INTO migrations (name) VALUES($1)`, name); err != nil {
+		return err
+	}
+	// Now that the file is migrated, commit the file
+	// If there is an error in the commit, it gets returned
+	return tx.Commit()
 }
